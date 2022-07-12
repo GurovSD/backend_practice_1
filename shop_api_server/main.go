337eaad93e0af465cfe0dadc81a42fb93afba747
GurovSD <gurov.sd@gmail.com>
2022-07-12 17:46:05 +0300

    added get/post handlers

--- shop_api_server/main.go
+++ shop_api_server/main.go
@@ -1,14 +1,79 @@
 package main
 
 import (
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
+type Handler struct {
+}
+
+type Employee struct {
+	Name   string  `json:"name" xml:"name"`
+	Age    int     `json:"age" xml:"age"`
+	Salary float32 `json:"salary" xml:"salary"`
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		name := r.FormValue("name")
+		fmt.Fprintf(w, "Parsed query-param with key \"name\": %s", name)
+
+	case http.MethodPost:
+		// body, err := ioutil.ReadAll(r.Body)
+		// if err != nil {
+		// 	http.Error(w, "Unable to read request body", http.StatusBadRequest)
+		// 	return
+		// }
+		// defer r.Body.Close()
+
+		var employee Employee
+		contentType := r.Header.Get("Content-Type")
+		switch contentType {
+		case "application/json":
+			err := json.NewDecoder(r.Body).Decode(&employee)
+			if err != nil {
+				http.Error(w, "Unable to unmarshal JSON", http.StatusBadRequest)
+				return
+			}
+		case "application/xml":
+			err := xml.NewDecoder(r.Body).Decode(&employee)
+			if err != nil {
+				http.Error(w, "Unable to unmarshal XML", http.StatusBadRequest)
+				return
+			}
+
+		default:
+			http.Error(w, "Unknown content type", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(w, "Got a new employee!\nName: %s\nAge: %dy.o.\nSalary %0.2f\n",
+			employee.Name,
+			employee.Age,
+			employee.Salary,
+		)
+	}
+}
+
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	handler := &Handler{}
+	// http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Hello World!")
+	// })
+	http.Handle("/", handler)
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
